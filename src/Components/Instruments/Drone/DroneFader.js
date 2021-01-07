acf2cefafc5b95078fb67170f0c7ede5f47ffe6b
Joshua Hume <valencelectron@hotmail.com>
2021-01-07 18:49:23 +0900

    drone

--- src/Components/Instruments/Drone/DroneFader.js
+++ src/Components/Instruments/Drone/DroneFader.js
@@ -1,27 +1,51 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import '../../../Styles/Components/Drone.css';
 
 const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
+const getNumber = (pitch) => {
+  let note;
+  pitch.includes('#') ? (note = pitch.substring(0, 2)) : (note = pitch[0]);
+  const octave = pitch[pitch.length - 1];
+  return NOTES.indexOf(note) + 12 * octave;
+};
+
 export default function DroneFader({ number, droneData, setDroneData, chordChange, setChordChange }) {
   const [note, setNote] = useState('C3');
+  const [pointerDown, setPointerDown] = useState(false);
+  const faderRef = useRef();
   const handleChange = (e) => {
-    const newPitch = `${NOTES[e.target.value % 12]}${Math.floor(e.target.value / 12)}`;
-    setNote(newPitch);
-    const copy = JSON.parse(JSON.stringify(droneData));
-    copy[number].pitch = newPitch;
-    setDroneData(copy);
+    if (!pointerDown) {
+      const newPitch = `${NOTES[e.target.value % 12]}${Math.floor(e.target.value / 12)}`;
+      setNote(newPitch);
+      const copy = JSON.parse(JSON.stringify(droneData));
+      copy[number].pitch = newPitch;
+      setDroneData(copy);
+    }
   };
   useEffect(() => {
-    if (chordChange) {
-      console.log('CHORD CHANGE!!!');
-      setChordChange(false);
-    }
-  }, [chordChange, setChordChange]);
+    faderRef.current.value = getNumber(droneData[number].pitch);
+    setChordChange(false);
+  }, [chordChange, setChordChange, number, droneData]);
+
   return (
     <div className="pitch-fader">
       <label htmlFor="range-fader">Pitch: </label>
-      <input type="range" name="range-fader" onChange={handleChange} min="12" max="72" defaultValue="36" />
+      <input
+        onPointerDown={() => {
+          setPointerDown(true);
+        }}
+        onPointerUp={() => {
+          setPointerDown(false);
+        }}
+        ref={faderRef}
+        type="range"
+        name="range-fader"
+        onChange={handleChange}
+        min="12"
+        max="72"
+        defaultValue="36"
+      />
       <label htmlFor="range-fader">{note}</label>
     </div>
   );
