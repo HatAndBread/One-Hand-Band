05791c72e15e9896d9eb341dde739da0c8db7566
Joshua Hume <valencelectron@hotmail.com>
2021-01-22 00:19:25 +0900

    split of settings and effects complete

--- src/Components/Instruments/SettingsLink.js
+++ src/Components/Instruments/SettingsLink.js
@@ -3,30 +3,25 @@ import '.././../Styles/Components/Settings.css';
 
 export default function SettingsLink({ forInstrument }) {
   const path = useLocation().pathname;
+  console.log(path);
   const capitalized = `${forInstrument[0].toUpperCase()}${forInstrument.substring(1, forInstrument.length)}`;
 
   return (
     <div className="settings-link-container">
-      {path === `/instrument/${forInstrument}` ? (
-        <div className="link-container">
-          <div>
-            <Link to={`${forInstrument}/settings`} className="settings-link">
-              🎚
-              <label style={{ fontSize: '16px' }}>Settings</label>
-            </Link>
-          </div>
-          <div>
-            <Link to={`${forInstrument}/effects`} className="settings-link">
-              🎛
-              <label style={{ fontSize: '16px' }}>Effects</label>
-            </Link>
-          </div>
+      <div className="link-container">
+        <div>
+          <Link to={`/instrument/${forInstrument}/settings`} className="settings-link">
+            🎚
+            <label style={{ fontSize: '16px' }}>Settings</label>
+          </Link>
         </div>
-      ) : (
-        <Link to={`/instrument/${forInstrument}`} className="hide-settings-btn link-btn">
-          Back ⏪
-        </Link>
-      )}
+        <div>
+          <Link to={`/instrument/${forInstrument}/effects`} className="settings-link">
+            🎛
+            <label style={{ fontSize: '16px' }}>Effects</label>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
