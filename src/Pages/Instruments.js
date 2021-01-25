6a8411b3f9b82da19f69dc2c11aa3167c89e83fa
Joshua Hume <valencelectron@hotmail.com>
2021-01-25 13:04:04 +0900

    fixed link highlighting

--- src/Pages/Instruments.js
+++ src/Pages/Instruments.js
@@ -1,60 +1,33 @@
 import { Link, Route } from 'react-router-dom';
-import { useContext, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { Context } from '../App';
 import InstrumentTemplate from '../Components/Instruments/InstrumentTemplate';
 import '../Styles/Components/Instruments.css';
 
-const onStyle = { boxShadow: '0 0 10px 5px #9d8df1', borderRadius: '50%', paddingLeft: '10px', paddingRight: '10px' };
+const onStyle = { boxShadow: '0 0 10px 5px #9d8df1', borderRadius: '50%' };
 export default function Instruments() {
-  const [keyboardStyle, setKeyboardStyle] = useState(undefined);
-  const [percStyle, setPercStyle] = useState(undefined);
-  const [droneStyle, setDroneStyle] = useState(undefined);
-  const [noiseStyle, setNoiseStyle] = useState(undefined);
   const myInstrument = useContext(Context).myInstrument;
-  const highlight = (e) => {
-    switch (e.target.innerText) {
-      case '🎹':
-        setKeyboardStyle(onStyle);
-        setPercStyle(null);
-        setDroneStyle(null);
-        setNoiseStyle(null);
-        break;
-      case '🥁':
-        setPercStyle(onStyle);
-        setKeyboardStyle(null);
-        setDroneStyle(null);
-        setNoiseStyle(null);
-        break;
-      case '🦥':
-        setDroneStyle(onStyle);
-        setPercStyle(null);
-        setKeyboardStyle(null);
-        setNoiseStyle(null);
-        break;
-      case '🙉':
-        setNoiseStyle(onStyle);
-        setPercStyle(null);
-        setDroneStyle(null);
-        setKeyboardStyle(null);
-        break;
-      default:
-        console.log('hi');
-    }
-  };
+  useEffect(() => {
+    console.log('!!!!!!!!!!!!!!!!!!1', myInstrument);
+  }, [myInstrument]);
+
   return (
     <div>
       <div>
         <div className="instrument-links">
-          <Link to="/instrument/keyboard" onClick={highlight} style={keyboardStyle}>
+          <Link to="/instrument/keyboard" style={myInstrument && myInstrument.instrument === 'keyboard' ? onStyle : {}}>
             🎹
           </Link>
-          <Link to="/instrument/percussion" onClick={highlight} style={percStyle}>
+          <Link
+            to="/instrument/percussion"
+            style={myInstrument && myInstrument.instrument === 'percussion' ? onStyle : {}}
+          >
             🥁
           </Link>
-          <Link to="/instrument/drone" onClick={highlight} style={droneStyle}>
+          <Link to="/instrument/drone" style={myInstrument && myInstrument.instrument === 'drone' ? onStyle : {}}>
             🦥
           </Link>
-          <Link to="/instrument/noise" onClick={highlight} style={noiseStyle}>
+          <Link to="/instrument/noise" style={myInstrument && myInstrument.instrument === 'noise' ? onStyle : {}}>
             🙉
           </Link>
         </div>
